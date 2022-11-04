aa73a9fd1c785fe8d0216a2141b4198af2b269ba
jeewanthamadu <jeewanthamadu2000@gmail.com>
2022-11-04 15:46:26 +0530

    create Translator utility

---
+++ src/main/java/lk/directpay/task/utility/Translator.java
@@ -0,0 +1,21 @@
+package lk.directpay.task.utility;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.stereotype.Component;
+import java.util.Locale;
+
+@Component
+public class Translator {
+    @Autowired
+    private static ResourceBundleMessageSource messageSource;
+
+    /*Translator(ResourceBundleMessageSource messageSource) {
+        Translator.messageSource = messageSource;
+    }*/
+
+    public static String toLocale(String msg) {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage(msg, null, locale);
+    }
+}
