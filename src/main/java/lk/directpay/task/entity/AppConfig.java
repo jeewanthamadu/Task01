dcadc422caafebb46046e8e9ee737dd71de3002a
jeewanthamadu <jeewanthamadu2000@gmail.com>
2022-11-04 14:07:25 +0530

    create AppConfig Entity

---
+++ src/main/java/lk/directpay/task/entity/AppConfig.java
@@ -0,0 +1,28 @@
+package lk.directpay.task.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
+public class AppConfig {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String platform;
+    private String minVersion;
+    private String latestVersion;
+    private String appUrl;
+    private String splashScreen;
+    private String authScreen;
+}
